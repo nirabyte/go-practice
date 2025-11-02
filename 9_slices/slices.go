f677bd8fe66e93381cfbc6a0db2df6143d0f8278
Nirakar Bogati <lmntrix.as619@gmail.com>
2025-11-02 21:21:49 +0545

    slices

--- 9_slices/slices.go
+++ 9_slices/slices.go
@@ -43,11 +43,11 @@ func main() {
 	// fmt.Println(nums[0:2])
 	// fmt.Println(nums[2:])
 
-	//slice
+	//slice package
 
-	var nums1 = []int{1, 2}
-	var nums2 = []int{1, 2}
-	fmt.Println(slices.Equal(nums1, nums2))
+	// var nums1 = []int{1, 2}
+	// var nums2 = []int{1, 2}
+	// fmt.Println(slices.Equal(nums1, nums2))
 
 	var nums = [][]int{{1, 3, 5}, {4, 5, 7}}
 	fmt.Println(nums)
