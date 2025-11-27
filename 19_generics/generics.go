2250b475ccb90374cccda2967ae8770c2c6d34d3
Nirakar Bogati <lmntrix.as619@gmail.com>
2025-11-27 14:15:13 +0545

    generics

--- 19_generics/generics.go
+++ 19_generics/generics.go
@@ -1,13 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func PrintSlice(items []int) {
+func PrintSlice[T any](items []T) { //if you want specific types to allow use T int | string
 	for _, item := range items {
 		fmt.Println(item)
 	}
 }
 
+//	func PrintStringSlice(items []string) {
+//		for _, item := range items {
+//			fmt.Println(item)
+//		}
+//	}
+type stack[T any] struct {
+	elements []T
+}
+
 func main() {
-	PrintSlice([]int{1,2,3})
+	// nums:= []int{1,2,3}
+
+	// names := []string{"golang", "java"}
+	// // PrintStringSlice(names)
+	// PrintSlice(names)
+	// PrintSlice(nums)
+	//LIFO
+myStack := stack[string]{
+	elements: []string{"go", "java"},
+}
+fmt.Println(myStack)
 }
